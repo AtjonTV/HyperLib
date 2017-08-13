49273c7b9758cd2928fcb2360e640303c9a8befa
AtjonTV <admin.atjontv@atvg-studios.at>
2017-08-13 18:31:04 +0200

    DevBuild 2 / v2.5.2 i71 [Unstable] {Added: CharServer-BaseStructure / Changed: Versioning of single components}

---
+++ HyperLib/HyperLib/ChatServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HyperLib
+{
+    public class ChatServer
+    {
+        private string ip;
+        private int port;
+        private TcpListener server;
+
+        public delegate void ClientConnect();
+        public event ClientConnect onClientConnect;
+
+        public String LocalAdress { get { return ip; }
+            set {
+                Regex reg = new Regex(@"^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$");
+                if (reg.IsMatch(value))
+                    ip = value;
+                else
+                    throw new InvalidOperationException("Wrong format from LocalAdress (IP)");
+            } }
+        public int Port { get { return port; } set { port = value; } }
+        public TcpListener getServer { get { return server; } }
+
+        public ChatServer(string locaAdress, int port)
+        {
+            ip = locaAdress;
+            this.port = port;
+            server = new TcpListener(IPAddress.Parse(ip), port);
+        }
+    }
+}
--- HyperLib/HyperLib/Information.cs
+++ HyperLib/HyperLib/Information.cs
@@ -9,22 +9,23 @@ namespace HyperLib
         public static String LibraryFramework       { get { return ".net Framework 4.5.2"; } }
         public static String LibraryVersion         { get { return "2.5.2"; } }
         public static int    LibraryVersionID       { get { return 71; } }
-        public static String XmlManagerVersion      { get { return "1.9"; } }
-        public static String EasyHashVersion        { get { return "1.9"; } }
-        public static String JsonManagerVersion     { get { return "1.8"; } }
-        public static String RedEngineVersion       { get { return "1.8"; } }
-        public static String ASCII_CheckVersion     { get { return "1.7"; } }
-        public static String BV_VEVersion           { get { return "1.7"; } }
-        public static String EasyCryptVersion       { get { return "1.5"; } }
-        public static String ASCIIVersion           { get { return "1.5"; } }
-        public static String AmmountVersion         { get { return "1.5"; } }
-        public static String BasicVehicleVersion    { get { return "1.5"; } }
-        public static String IntegetSortVersion     { get { return "1.4"; } }
+        public static String RedEngineVersion       { get { return "1.5"; } }
+        public static String EasyHashVersion        { get { return "1.4"; } }
         public static String GeneratorVersion       { get { return "1.4"; } }
-        public static String BooleanVersion         { get { return "1.3"; } }
-        public static String DoubleVersion          { get { return "1.3"; } }
-        public static String LetterVersion          { get { return "1.3"; } }
-        public static String IntegerVersion         { get { return "1.2"; } }
+        public static String IntegetSortVersion     { get { return "1.3"; } }
+        public static String XmlManagerVersion      { get { return "1.3"; } }
+        public static String JsonManagerVersion     { get { return "1.2"; } }
+        public static String AmmountVersion         { get { return "1.2"; } }
+        public static String ChatServerVersion      { get { return "1.0"; } }
+        public static String ASCII_CheckVersion     { get { return "1.0"; } }
+        public static String BV_VEVersion           { get { return "1.0"; } }
+        public static String EasyCryptVersion       { get { return "1.0"; } }
+        public static String ASCIIVersion           { get { return "1.0"; } }
+        public static String BasicVehicleVersion    { get { return "1.0"; } }
+        public static String BooleanVersion         { get { return "1.0"; } }
+        public static String DoubleVersion          { get { return "1.0"; } }
+        public static String LetterVersion          { get { return "1.0"; } }
+        public static String IntegerVersion         { get { return "1.0"; } }
         public static String GeneratorTypeVersion   { get { return "1.0"; } }
         public static String GeneratorCharsetVersion{ get { return "1.0"; } }
     }
